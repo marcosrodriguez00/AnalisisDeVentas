7a7d91e94c4129b1b4351f88ba9a5cc6068fc09a
Marcos Rodríguez <119641901+marcosrodriguez00@users.noreply.github.com>
2025-09-01 10:36:57 -0300

    Update funcionalidades.py

--- funcionalidades.py
+++ funcionalidades.py
@@ -1,26 +1,35 @@
+# DESCRIPCIÓN DE LA FUNCIÓN #
 def crecimientoVentas:
     return 0
 
+# DESCRIPCIÓN DE LA FUNCIÓN #
 def productosMasVendidos:
     return 0
 
+# DESCRIPCIÓN DE LA FUNCIÓN #
 def clientesMasRelevantes:
     return 0
 
+# DESCRIPCIÓN DE LA FUNCIÓN #
 def TicketPromedioDeVenta:
     return 0
 
+# DESCRIPCIÓN DE LA FUNCIÓN #
 def VentasPorPeriodo:
     return 0
 
+# DESCRIPCIÓN DE LA FUNCIÓN #
 def ComparativaProducto:
     return 0
 
+# DESCRIPCIÓN DE LA FUNCIÓN #
 def ComparativaCliente:
     return 0
 
+# DESCRIPCIÓN DE LA FUNCIÓN #
 def ComparativaRegion:
     return 0
 
+# DESCRIPCIÓN DE LA FUNCIÓN #
 def tendenciaDeCrecimiento:
     return 0
